251f5e82702973225388828c187dfa2daa4cb55b
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-09-24 19:21:52 -0300

    agregamos las rutas

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,7 +1,10 @@
+import {Button} from "react-bootstrap"
+
 const Error404 = () => {
     return (
         <div>
-            
+         <h1>Error 404</h1> 
+         <Button variant='warning'>Volver al Inicio</Button>  
         </div>
     );
 };
